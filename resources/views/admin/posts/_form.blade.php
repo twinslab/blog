9c1983e8db1493ea4ae02bb241313cb052cd244d
Antonis Gr <grigant12@yahoo.gr>
2015-05-24 18:52:14 +0300

    form error feedback

--- resources/views/admin/posts/_form.blade.php
+++ resources/views/admin/posts/_form.blade.php
@@ -1,20 +1,24 @@
-<div class="form-group">
+<div class="form-group {!! !$errors->first('title') ?: 'has-error' !!}">
+    {!! $errors->first('title', '<span class="text-muted pull-right">:message</span>') !!}
     {!! Form::label('title', 'Title:') !!}
     {!! Form::text('title', isset($post->title)?$post->title:null, ['class' => 'form-control']) !!}
 </div>
 
-<div class="form-group">
+<div class="form-group {!! !$errors->first('slug') ?: 'has-error' !!}">
+    {!! $errors->first('slug', '<span class="text-muted pull-right">:message</span>') !!}
     {!! Form::label('slug', 'Slug:') !!}
     {!! Form::text('slug', isset($post->slug)?$post->slug:null, ['class' => 'form-control']) !!}
 </div>
 
-<div class="form-group">
+<div class="form-group {!! !$errors->first('content_md') ?: 'has-error' !!}">
+    {!! $errors->first('content_md', '<span class="text-muted pull-right">:message</span>') !!}
     {!! Form::label('content_md', 'Content:') !!}
     {!! Form::textarea('content_md', isset($post->content_md)?$post->content_md:null, ['class' => 'form-control', 'rows' => 15]) !!}
 </div>
 
 @unless (empty($tags))
-    <div class="form-group">
+    <div class="form-group {!! !$errors->first('tags') ?: 'has-error' !!}">
+        {!! $errors->first('tags', '<span class="text-muted pull-right">:message</span>') !!}
         {!! Form::label('tags', 'Tags:') !!}
         <select name="tags[]" id="tags" class="form-control" multiple>
             @foreach ($tags as $id => $name)
@@ -35,7 +39,7 @@
 
     <script>
         // select2 tags
-        var placeholder = "Select a tag...";
+        var placeholder = "Select tags...";
         $( "#tags" ).select2( { placeholder: placeholder } );
 
         // markdown editor
